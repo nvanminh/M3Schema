4f5b0998d31a95fb496ab0d072dc7822b10a0a73
Jackie.Nguyen <nvanminh@tma.com.vn>
2016-11-18 13:47:37 +0700

    Add source code

---
+++ mainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SchemaApplication
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnFileBrowser = new System.Windows.Forms.Button();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.btnTransform = new System.Windows.Forms.Button();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.btnFolderBrowser = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "FileOpen";
+            // 
+            // btnFileBrowser
+            // 
+            this.btnFileBrowser.Location = new System.Drawing.Point(445, 32);
+            this.btnFileBrowser.Name = "btnFileBrowser";
+            this.btnFileBrowser.Size = new System.Drawing.Size(45, 20);
+            this.btnFileBrowser.TabIndex = 1;
+            this.btnFileBrowser.Text = "...";
+            this.btnFileBrowser.UseVisualStyleBackColor = true;
+            this.btnFileBrowser.Click += new System.EventHandler(this.btnFileBrowser_Click);
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Location = new System.Drawing.Point(109, 32);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.Size = new System.Drawing.Size(330, 20);
+            this.tbFilePath.TabIndex = 2;
+            // 
+            // btnTransform
+            // 
+            this.btnTransform.Location = new System.Drawing.Point(109, 102);
+            this.btnTransform.Name = "btnTransform";
+            this.btnTransform.Size = new System.Drawing.Size(103, 23);
+            this.btnTransform.TabIndex = 4;
+            this.btnTransform.Text = "Transform";
+            this.btnTransform.UseVisualStyleBackColor = true;
+            this.btnTransform.Click += new System.EventHandler(this.btnTransform_Click);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(34, 32);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(41, 13);
+            this.lblSource.TabIndex = 5;
+            this.lblSource.Text = "Source";
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(34, 65);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(60, 13);
+            this.lblDestination.TabIndex = 6;
+            this.lblDestination.Text = "Destination";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(109, 65);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.Size = new System.Drawing.Size(330, 20);
+            this.txtDestination.TabIndex = 7;
+            // 
+            // btnFolderBrowser
+            // 
+            this.btnFolderBrowser.Location = new System.Drawing.Point(446, 65);
+            this.btnFolderBrowser.Name = "btnFolderBrowser";
+            this.btnFolderBrowser.Size = new System.Drawing.Size(44, 20);
+            this.btnFolderBrowser.TabIndex = 8;
+            this.btnFolderBrowser.Text = "...";
+            this.btnFolderBrowser.UseVisualStyleBackColor = true;
+            this.btnFolderBrowser.Click += new System.EventHandler(this.btnFolderBrowser_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(222, 102);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(103, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(336, 102);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 149);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnFolderBrowser);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.btnTransform);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.btnFileBrowser);
+            this.Name = "mainForm";
+            this.Text = "Transform Database Server Schema";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnFileBrowser;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.Button btnTransform;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Button btnFolderBrowser;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SchemaApplication
+{
+    public partial class mainForm : Form
+    {
+        string appPath = Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+        string basePath = ConfigurationManager.AppSettings["MigrationScriptDirectory"];
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        public mainForm()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            ofd.InitialDirectory = basePath;
+            //ofd.CustomPlaces.Add(@"C:\");
+            ofd.Title = "Pick a sql script file";
+            ofd.Filter = "SQL Scripts|*.sql";
+
+            txtDestination.Text = appPath;
+        }
+        public string selectedFilePath
+        {
+            get
+            {
+                return tbFilePath.Text;
+            }
+            set
+            {
+                tbFilePath.Text = value;
+            }
+        }
+        private void ChooseFile()
+        {
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK && ofd.FileName.Length > 0)
+            {
+                selectedFilePath = ofd.FileName;
+            }
+        }
+        private void ChooseFolder()
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtDestination.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+        private string ReadFileIntoStringCollection()
+        {
+            const int MaxBytes = 65536;
+            StreamReader sr = new StreamReader(selectedFilePath);
+            StringBuilder result = new StringBuilder();
+            int nBytesRead = 0;
+            string nextLine;
+            while ((nextLine = sr.ReadLine()) != null)
+            {
+                nBytesRead += nextLine.Length;
+                if (nBytesRead > MaxBytes)
+                    break;
+                result.AppendLine(nextLine);
+            }
+            sr.Close();
+            return result.ToString();
+        }
+        private void CreateFileFromStream(string filePath, Stream stream)
+        {
+            int bufferSize = (int)stream.Length;
+            if (bufferSize == 0) return;
+            using (FileStream fileStream = System.IO.File.Create(filePath, bufferSize))
+            {
+                byte[] buffer = new byte[bufferSize];
+                stream.Read(buffer, 0, Convert.ToInt32(buffer.Length));
+                fileStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        private void TransformSchema()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(selectedFilePath))
+                {
+                    MessageBox.Show("Please select a file", "Warning");
+                }
+
+                var fileInfo = new FileInfo(selectedFilePath);
+                if (!fileInfo.Exists)
+                {
+                    MessageBox.Show("File is not existed", "Error");
+                }
+
+                var args = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase) {
+                {"M3_Dev1.", "M3_Test1."},
+                {"M3_Dev2.", "M3_Test2."},
+                {"[M3_Dev1].", "[M3_Test1]."},
+                {"[M3_Dev2].", "[M3_Test2]."}
+            };
+
+                string source = ReadFileIntoStringCollection();
+                string output = args.Aggregate(source, (current, value) => current.Replace(value.Key, value.Value));
+
+                string targetPath = txtDestination.Text;
+                var fileName = Path.GetFileNameWithoutExtension(fileInfo.Name);
+                var newFilePath = Path.Combine(targetPath, string.Format("{0}-edited{1}", fileName, fileInfo.Extension));
+
+                FileStream fileStream = new FileStream(newFilePath, FileMode.OpenOrCreate, FileAccess.Write);
+                using (StreamWriter sw = new StreamWriter(fileStream))
+                {
+                    // write a line of text to the file
+                    sw.WriteLine(output);
+                    sw.Close();
+
+                    MessageBox.Show("Transform Successfully");
+                    //this.Close();
+
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return;
+            }
+        }
+
+        private void btnFileBrowser_Click(object sender, EventArgs e)
+        {
+            ChooseFile();
+        }
+        private void btnFolderBrowser_Click(object sender, EventArgs e)
+        {
+            ChooseFolder();
+        }
+        private void btnTransform_Click(object sender, EventArgs e)
+        {
+            TransformSchema();
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("This will close down the whole application. Confirm?", "Close Application", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            
+            txtDestination.Text = "";
+            openFileDialog.Reset();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
